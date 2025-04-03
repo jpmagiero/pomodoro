53415cb38e75c74de8ed4d066a14672a48144f5c
jpmagiero <jpaulomr12@gmail.com>
2025-04-02 23:07:00 -0300

    :sparkles: add icon from lucide-react and update heading component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading attr={123} attr2={'String'}>
+      <Heading>
         Hello World!
+        <button>
+          <TimerIcon />
+        </button>
       </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
