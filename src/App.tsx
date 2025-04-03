f995fbb7ac7aac5d6c2989ed23f84954e333828e
jpmagiero <jpaulomr12@gmail.com>
2025-04-03 17:05:44 -0300

    :sparkles: update global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
-
 export function App() {
   return (
     <>
-      <Heading>
-        Hello World!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-        consectetur ad est debitis praesentium aliquam tempore commodi eum porro
-        perferendis magnam voluptate odit, culpa voluptas officia consequatur
-        molestiae. Quo, veritatis?
-      </p>
+      <div className='conteiner'>
+        <div className='content'>
+          <section>Logo</section>
+        </div>
+      </div>
+
+      <div className='conteiner'>
+        <div className='content'>
+          <section>Menu</section>
+        </div>
+      </div>
+
+      <div className='conteiner'>
+        <div className='content'>
+          <section>Formulário</section>
+        </div>
+      </div>
+
+      <div className='conteiner'>
+        <div className='content'>
+          <section>Footer</section>
+        </div>
+      </div>
     </>
   );
 }
